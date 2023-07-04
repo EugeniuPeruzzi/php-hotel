fedcb8605022ef0199f30601eb9d6a3bfe5f1f7a
EugeniuPeruzzi <jaka1997@live.it>
2023-07-04 17:16:15 +0200

    base -> fixed [bonus 1 ] -> fixed

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-
+    //<?php ? >
+    include __DIR__.'/partials/variables.php';
 
 ?>
 <!-- Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G Stampare tutti i nostri hotel con tutti i dati disponibili. Iniziate in modo graduale. 
@@ -17,6 +18,37 @@ Prima stampate in pagina i dati, senza preoccuparvi dello stile. Dopo aggiungete
     <title>Document</title>
 </head>
 <body>
-    <?php ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 ">
+                <div class="table-content mt-5 text-center">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Descrizione struttura</th>
+                                <th scope="col">parcheggio</th>
+                                <th scope="col">Voto</th>
+                                <th scope="col">Distanza dal centro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php  foreach ($hotels as $hotel) { ?>
+                                <tr>
+                                    <td class="p-3"><?php echo $hotel['name'] ?></td>
+                                    <td class="p-3"><?php echo $hotel['description'] ?></td>
+                                    <td class="p-3"><?php echo $hotel['parking'] ?></td> <!-- parcheggio non lo stampa visto che e un valore booleano -->
+                                    <td class="p-3"><?php echo $hotel['vote'] ?></td>
+                                    <td class="p-3"><?php echo $hotel['distance_to_center']?>km</td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+   
 </body>
 </html>
+
